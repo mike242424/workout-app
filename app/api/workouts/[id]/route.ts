5dfc157e6da94ceaefe3a10ff15c2f0fd386d829
Michael Byers <mikebyers24@gmail.com>
2024-06-17 15:16:47 -0400

    update workout enpoint with zod validation.

--- app/api/workouts/[id]/route.ts
+++ app/api/workouts/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/db';
 import { paramsIdSchema } from '@/validation/paramsIdSchema';
+import { workoutSchema } from '@/validation/workoutSchema';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function DELETE(
@@ -41,6 +42,67 @@ export async function DELETE(
     });
 
     return NextResponse.json({ message: 'Workout deleted successfully.' });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal server error.' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } },
+) {
+  try {
+    const validateId = paramsIdSchema.safeParse({ id });
+
+    if (!validateId.success) {
+      return NextResponse.json(
+        { error: validateId.error.errors[0].message },
+        { status: 403 },
+      );
+    }
+
+    const { title } = await req.json();
+
+    if (!title) {
+      return NextResponse.json(
+        { error: 'Invalid request. Title required.' },
+        { status: 400 },
+      );
+    }
+
+    const validateTitle = workoutSchema.safeParse({ title });
+
+    if (!validateTitle.success) {
+      return NextResponse.json(
+        {
+          error: validateTitle.error.errors[0].message,
+        },
+        { status: 403 },
+      );
+    }
+
+    const workout = await prisma.workout.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!workout) {
+      return NextResponse.json(
+        { error: 'Workout not found.' },
+        { status: 404 },
+      );
+    }
+
+    const updatedWorkout = await prisma.workout.update({
+      where: { id },
+      data: { title },
+    });
+
+    return NextResponse.json(updatedWorkout);
   } catch (error) {
     return NextResponse.json(
       { error: 'Internal server error.' },
