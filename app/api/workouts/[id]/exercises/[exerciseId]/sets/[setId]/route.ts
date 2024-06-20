71d9b589c2c858584defc44b2922b1eb251243b3
Michael Byers <mikebyers24@gmail.com>
2024-06-20 10:42:36 -0400

    delete set endpoint with zod validation.

--- app/api/workouts/[id]/exercises/[exerciseId]/sets/[setId]/route.ts
+++ app/api/workouts/[id]/exercises/[exerciseId]/sets/[setId]/route.ts
@@ -62,11 +62,102 @@ export async function GET(
     const set = await prisma.set.findUnique({
       where: {
         id: setId,
+        exerciseId,
       },
     });
 
-    return NextResponse.json(set)
+    if (!set) {
+      return NextResponse.json({ error: 'Set not found.' });
+    }
+
+    return NextResponse.json(set);
   } catch (error) {
-    return NextResponse.json({ error: 'Internal server error.' });
+    return NextResponse.json(
+      { error: 'Internal server error.' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  {
+    params: { id: workoutId, exerciseId, setId },
+  }: { params: { id: string; exerciseId: string; setId: string } },
+) {
+  try {
+    if (!workoutId) {
+      return NextResponse.json(
+        { error: 'Invalid request. Workout id required.' },
+        { status: 400 },
+      );
+    }
+
+    const validateWorkoutId = paramsWorkoutIdSchema.safeParse({ workoutId });
+
+    if (!validateWorkoutId.success) {
+      return NextResponse.json(
+        { error: validateWorkoutId.error.errors[0].message },
+        { status: 403 },
+      );
+    }
+
+    if (!exerciseId) {
+      return NextResponse.json(
+        { error: 'Invalid request. Exercise id required.' },
+        { status: 400 },
+      );
+    }
+
+    const validateExerciseId = paramsExerciseIdSchema.safeParse({ exerciseId });
+
+    if (!validateExerciseId.success) {
+      return NextResponse.json(
+        { error: validateExerciseId.error.errors[0].message },
+        { status: 403 },
+      );
+    }
+
+    if (!workoutId) {
+      return NextResponse.json(
+        { error: 'Invalid request. Workout id required.' },
+        { status: 400 },
+      );
+    }
+
+    const validateSetId = paramsSetIdSchema.safeParse({ setId });
+
+    if (!validateSetId.success) {
+      return NextResponse.json(
+        { error: validateSetId.error.errors[0].message },
+        { status: 403 },
+      );
+    }
+
+    const set = await prisma.set.findUnique({
+      where: {
+        id: setId,
+        exerciseId,
+      },
+    });
+
+    if (!set) {
+      return NextResponse.json({ error: 'Set not found.' });
+    }
+
+    const deletedSet = await prisma.set.delete({
+      where: {
+        id: setId,
+        exerciseId,
+      },
+    });
+
+    return NextResponse.json({ messsage: 'Set deleted successfully.' });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: 'Internal server error.' },
+      { status: 500 },
+    );
   }
 }
