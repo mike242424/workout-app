3228a9ef30f312df90a22798904e089d4b75669b
Michael Byers <mikebyers24@gmail.com>
2024-06-23 18:10:17 -0400

    navbar text styles.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,17 +4,18 @@ import { getServerSession } from 'next-auth';
 
 const Navbar = async () => {
   const session = await getServerSession();
+  console.log(session?.user);
 
   return (
     <nav className="bg-gradient-to-r from-sky-500 to-sky-800 p-4">
       <div className="flex justify-between items-center">
-        <Link className="font-bold text-4xl " href="/">
+        <Link className="font-bold text-4xl text-white" href="/">
           Workout App
         </Link>
 
         {session && (
           <div className="flex justify-center items-center gap-2">
-            <Link className="font-bold" href="/workouts">
+            <Link className="font-bold text-white" href="/workouts">
               Workouts
             </Link>
             <LogoutButton />
