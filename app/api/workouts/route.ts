0bd97123c13e7e7a9759608104dd405889a1b262
Michael Byers <mikebyers24@gmail.com>
2024-06-23 17:21:51 -0400

    using getToken to only reteieve the users workouts.

--- app/api/workouts/route.ts
+++ app/api/workouts/route.ts
@@ -1,9 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
+import { getToken } from 'next-auth/jwt';
 import { workoutSchema } from '@/validation/workoutSchema';
 
 export async function POST(req: NextRequest) {
   try {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    if (!token || !token.email) {
+      return NextResponse.json({ error: 'Unauthorized.' }, { status: 401 });
+    }
+
     const { title } = await req.json();
 
     if (!title) {
@@ -22,16 +29,25 @@ export async function POST(req: NextRequest) {
       );
     }
 
+    const user = await prisma.user.findUnique({
+      where: { email: token.email },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found.' }, { status: 404 });
+    }
+
     const newWorkout = await prisma.workout.create({
       data: {
         title,
+        userId: user.id,
       },
     });
 
     return NextResponse.json(newWorkout, { status: 201 });
   } catch (error) {
     return NextResponse.json(
-      { error: 'Internal server error.' },
+      { error: 'Internal server error' },
       { status: 500 },
     );
   }
@@ -39,10 +55,23 @@ export async function POST(req: NextRequest) {
 
 export async function GET(req: NextRequest) {
   try {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    if (!token || !token.email) {
+      return NextResponse.json({ error: 'Unauthorized.' }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email: token.email },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found.' }, { status: 404 });
+    }
+
     const workouts = await prisma.workout.findMany({
-      orderBy: {
-        createdAt: 'desc',
-      },
+      where: { userId: user.id },
+      orderBy: { createdAt: 'desc' },
     });
 
     return NextResponse.json(workouts);
