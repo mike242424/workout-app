b6be4afa4bf970c4e77b47c62225409d5efe52ef
Michael Byers <mikebyers24@gmail.com>
2024-06-23 13:31:43 -0400

    register user form client page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import RegisterUserForm from '@/components/register-user-form';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
 const HomePage = () => {
   return (
-    <main>
-      <h1>Workout App</h1>
+    <main className="flex items-center justify-center mt-20">
+      <Card className="w-10/12 md:w-8/12 lg:w-6/12 hover:shadow-xl">
+        <CardHeader>
+          <CardTitle className="font-bold text-3xl mt-4 text-center">
+            Register
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <RegisterUserForm />
+        </CardContent>
+      </Card>
     </main>
   );
 };
---
+++ components/register-user-form.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+type RegisterUserFormData = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { User } from '@prisma/client';
+import axios from 'axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { registerUserSchema } from '@/validation/registerUserSchema';
+
+const RegisterUserForm = () => {
+  const queryClient = useQueryClient();
+
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(registerUserSchema),
+    defaultValues: { username: '', email: '', password: '' },
+  });
+  const mutation = useMutation({
+    mutationFn: registerUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] });
+      router.push('/login');
+    },
+  });
+
+  async function registerUser(data: RegisterUserFormData) {
+    const response = await axios.post(
+      'http://localhost:3000/api/auth/users/register',
+      data,
+    );
+    return response.data;
+  }
+
+  const handleFormSubmit = (data: RegisterUserFormData) => {
+    mutation.mutate(data);
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <Form {...form}>
+        <form
+          className="flex flex-col w-6/12 gap-4"
+          onSubmit={form.handleSubmit(handleFormSubmit)}
+        >
+          <FormField
+            name="username"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username:</FormLabel>
+                <FormControl>
+                  <Input {...field} type="text" placeholder="Username" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            name="email"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email:</FormLabel>
+                <FormControl>
+                  <Input {...field} type="text" placeholder="Email" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password:</FormLabel>
+                <FormControl>
+                  <Input {...field} type="password" placeholder="********" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <Button type="submit">Register</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default RegisterUserForm;
