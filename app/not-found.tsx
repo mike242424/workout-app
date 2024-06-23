39eb1d99b5eccf6fcbce29b1948df422e8020ff1
Michael Byers <mikebyers24@gmail.com>
2024-06-23 14:44:18 -0400

    404 not found page.

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <main className="flex flex-col items-center justify-center gap-4 mt-10">
+      <h1 className="font-bold text-3xl">Page Not Found</h1>
+      <p>
+        This page does not exist. Return
+        <Link className="font-bold hover:underline ml-1" href="workouts">
+          home?
+        </Link>
+      </p>
+    </main>
+  );
+};
+
+export default NotFound;
