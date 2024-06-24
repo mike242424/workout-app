27f683d2120e1013ce4808fadbf8842e87f6d8c4
Michael Byers <mikebyers24@gmail.com>
2024-06-24 15:19:53 -0400

    updated next.config for deployment.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['v2.exercisedb.io'],
   },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXT_PUBLIC_EXERCISEDB_API_KEY: process.env.NEXT_PUBLIC_EXERCISEDB_API_KEY,
+    NEXT_PUBLIC_EXERCISEDB_HOST: process.env.NEXT_PUBLIC_EXERCISEDB_HOST,
+  },
 };
 
 export default nextConfig;
